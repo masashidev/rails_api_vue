ca3021aa796bf5738e248e2b12d8316a641060c8
Masashi.i <masashi.itamura.spt90@gmail.com>
2024-03-21 14:55:42 +0900

    prepare Posts view

---
+++ client/src/components/Posts.vue
@@ -0,0 +1,32 @@
+
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Posts</h1>
+    <input type="text"
+        class="category-input"
+        placeholder="Category"
+        v-model="category"
+    />
+    <input type="text"
+        class="body-input"
+        placeholder="Body"
+        v-model="body"
+    />
+
+
+  </div>
+</template>
+
+<style scoped>
+  .category-input{
+    width: 
+  }
+  .body-input{
+
+  }
+
+</style>
